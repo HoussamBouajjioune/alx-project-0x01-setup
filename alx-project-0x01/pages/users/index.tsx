0ffcb53fbd9fce97bec07abf8fb3a82e01c8efed
houssam bouajjioune <houssambouajjioune1998@gmail.com>
2025-05-30 20:52:02 +0100

    Refactor getStaticProps in Users component for improved readability

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -21,19 +21,15 @@ const Users: React.FC<{ users: UserProps[] }> = ({ users }) => {
   );
 };
 
-
-
 export async function getStaticProps() {
-  const response = await fetch("https://jsonplaceholder.typicode.com/users")
-  const users = await response.json()
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users = await response.json();
 
   return {
     props: {
-      users
-    }
-  }
+      users,
+    },
+  };
 }
-
-
 
 export default Users;
